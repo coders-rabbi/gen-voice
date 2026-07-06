d8d62bc9aa7da214f051e3d043733fafc449e71a
codersrabbi <pcrabbi2020@gmail.com>
2026-07-06 12:54:16 +0600

    writer page done

--- src/components/ui/home/components/writesCard.tsx
+++ src/components/ui/home/components/writesCard.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 const WritesCard = () => {
   return (
-    <div className="flex items-center gap-2  shadow-2xl p-1.5 w-full rounded-xl">
+    <div className="flex items-center gap-2 border shadow-xl p-1.5 w-full rounded-[12px]">
       <Image src={img} alt="gen voice" className="h-20 w-20 object-cover" />
       <div>
         <div className="flex justify-between mb-2">
