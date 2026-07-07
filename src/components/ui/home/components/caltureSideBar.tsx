bc2f93acfc0d44efca689c9d389f3bc8d6c06f9a
codersrabbi <pcrabbi2020@gmail.com>
2026-07-07 13:05:44 +0600

    calture news sidebar responsive done

--- src/components/ui/home/components/caltureSideBar.tsx
+++ src/components/ui/home/components/caltureSideBar.tsx
@@ -35,7 +35,7 @@ const CaltureSideBar = () => {
       </div>
 
       <div className="mt-4">
-        <div className="md:flex gap-0.5 mb-2">
+        <div className="flex gap-0.5 mb-2">
           <Link href="/">
             <div>
               <h5 className="text-[#6D757F] text-xs font-semibold">
@@ -55,7 +55,8 @@ const CaltureSideBar = () => {
             className="w-24 h-24 object-cover"
           />
         </div>
-        <div className="md:flex gap-0.5 mb-2">
+        <hr className="my-2"/>
+        <div className="flex gap-0.5 mb-2">
           <Link href="/">
             <div>
               <h5 className="text-[#6D757F] text-xs font-semibold">
@@ -75,46 +76,6 @@ const CaltureSideBar = () => {
             className="w-24 h-24 object-cover"
           />
         </div>
-        {/* <div className="md:flex gap-0.5 mb-2">
-          <Link href="/">
-            <div>
-              <h5 className="text-[#6D757F] text-xs font-semibold">
-                BASKET BALL
-              </h5>
-              <h3 className="text-[16px] text-[#183354] font-semibold">
-                Using Instagram tawo promote your
-              </h3>
-              <p className="flex items-center gap-1 text-xs text-[#6D757F] font-semibold text-xs mt-2.5">
-                <CiCalendar /> 27 Jun, 2026
-              </p>
-            </div>
-          </Link>
-          <Image
-            src={football}
-            alt="gen voice"
-            className="w-24 h-24 object-cover"
-          />
-        </div>
-        <div className="md:flex gap-0.5">
-          <Link href="/">
-            <div>
-              <h5 className="text-[#6D757F] text-xs font-semibold">
-                BASKET BALL
-              </h5>
-              <h3 className="text-[16px] text-[#183354] font-semibold">
-                Using Instagram tawo promote your
-              </h3>
-              <p className="flex items-center gap-1 text-xs text-[#6D757F] font-semibold text-xs mt-2.5">
-                <CiCalendar /> 27 Jun, 2026
-              </p>
-            </div>
-          </Link>
-          <Image
-            src={basket}
-            alt="gen voice"
-            className="w-24 h-24 object-cover"
-          />
-        </div> */}
       </div>
     </div>
   );
