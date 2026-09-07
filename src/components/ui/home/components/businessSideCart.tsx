eb4ffb0507ed426302861041c646f595488841b0
codersrabbi <pcrabbi2020@gmail.com>
2026-09-07 16:56:33 +0600

    done

--- src/components/ui/home/components/businessSideCart.tsx
+++ src/components/ui/home/components/businessSideCart.tsx
@@ -1,23 +1,24 @@
-import { IPost } from "@/types/news";
+import { TNews } from "@/types/news";
 import Link from "next/link";
-import React from "react";
 import { CiCalendar } from "react-icons/ci";
 
 interface porstProps {
-  posts: IPost;
+  posts: TNews;
 }
 
 const BusinessSideCart = ({ posts }: porstProps) => {
   return (
     <div>
-      <Link href={`news/${posts?.postId}`}>
+      <Link href={`news/${posts?.newsId}`}>
         <div className="border-b pb-1.5 mt-1.5">
-          <p className="text-xs text-[#6D757F] font-semibold">{posts?.category}</p>
+          <p className="text-xs text-[#6D757F] font-semibold">
+            {posts?.categoryId?.categoryName}
+          </p>
           <h4 className="text-xs text-[#183354] font-bold mt-1">
             {posts?.title}
           </h4>
           <p className="flex items-center gap-1 text-xs text-[#6D757F] font-semibold mt-2">
-            <CiCalendar /> {posts?.postDate}
+            <CiCalendar /> {posts?.publishAt?.split("T")[0]}
           </p>
         </div>
       </Link>
